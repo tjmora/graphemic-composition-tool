ac5493e34d460245f8a5a75f7489a2822c41ae65
Tyrel Jonas Mora <tyreljonas.mora@gmail.com>
2020-10-07 05:53:56 +0800

    completed

---
+++ README.md
@@ -0,0 +1,7 @@
+## About
+
+A simple online tool for viewing the code point sequence of a text, and precisely manipulating the text by inserting characters and code points (like diacritics) into precise positions in the text. This tool also has buttons to normalize the text in any of the four normalization forms.
+
+## License
+
+[MIT](LICENSE)
--- index.js
+++ index.js
@@ -1,56 +1,127 @@
+let charsText, codesText, selectBlock, charsBlock;
+
 function codesCursor () {
     if (document.getElementById("charsText").value === "")
         return
-    let ta = document.getElementById("charsText");
     let cas = document.querySelectorAll("#codesText span");
     cas.forEach((sp) => {
         sp.style.borderRight = "";
         sp.style.backgroundColor = "";
     });
-    if (ta.selectionStart === ta.selectionEnd) {
-        let codesBefore = [...ta.value.slice(0, ta.selectionStart)].length;
-        cas[codesBefore - 1].style.borderRight = "solid 1px rgba(0,0,0,1)";
+    if (charsText.selectionStart === charsText.selectionEnd) {
+        let codesBefore = [...charsText.value.slice(0, charsText.selectionStart)].length;
+        cas[codesBefore - 1].style.borderRight = "solid 2px rgba(0,127,0,1)";
     }
     else {
-        let codesTotal = [...ta.value].length;
-        let codesBefore = [...ta.value.slice(0, ta.selectionStart)].length;
-        let codesAfter = [...ta.value.slice(ta.selectionEnd)].length;
+        let codesTotal = [...charsText.value].length;
+        let codesBefore = [...charsText.value.slice(0, charsText.selectionStart)].length;
+        let codesAfter = [...charsText.value.slice(charsText.selectionEnd)].length;
         for (let i = codesBefore, l = codesTotal-codesAfter; i<l; i++)
-            cas[i].style.backgroundColor = "green";
+            cas[i].style.backgroundColor = "rgba(0,127,0,1)";
     }
 }
 
+function moveCursor (index) {
+    charsText.focus();
+    charsText.selectionStart = charsText.selectionEnd = [...charsText.value].slice(0, index+1).join("").length;
+    codesCursor();
+}
+
 function codesRender () {
-    document.getElementById("codesText").innerHTML 
-        = Array.from(document.getElementById("charsText").value)
-            .map(ch => "<span>"+ch.codePointAt(0).toString(16)+"</span>").join("");
+    codesText.innerHTML = ""
+    Array.from(charsText.value).map((ch, i) => {
+        let span = document.createElement("span");
+        span.textContent = ch.codePointAt(0).toString(16);
+        span.addEventListener("click", () => {
+            moveCursor(i);
+        });
+        codesText.appendChild(span);
+    });
+}
+
+function insertChar (char) {
+    let start = charsText.selectionStart;
+    charsText.value = charsText.value.slice(0, start) + char 
+                        + charsText.value.slice(charsText.selectionEnd);
+    charsText.focus();
+    charsText.selectionStart = start + 1;
+    charsText.selectionEnd = start + 1;
+    codesRender();
+}
+
+function renderChars () {
+    charsBlock.innerHTML = "";
+    let start = parseInt(selectBlock.options[selectBlock.selectedIndex].value, 16);
+    let end = selectBlock.selectedIndex < selectBlock.options.length - 1
+                ? parseInt(selectBlock.options[selectBlock.selectedIndex + 1].value, 16)
+                : 0x10000
+    while (start < end) {
+        let row = document.createElement("tr");
+        for (let i = 0; i < 16; i++) {
+            let cell = document.createElement("td");
+            let ch = String.fromCodePoint(start + i);
+            cell.textContent = ch;
+            cell.addEventListener("click", () => {
+                insertChar(ch);
+            });
+            row.appendChild(cell);
+        }
+        charsBlock.appendChild(row);
+        start += 16
+    }
 }
 
 window.onload = () => {
 
-    document.getElementById("charsText").addEventListener("click", codesCursor);
-    //document.getElementById("charsText").addEventListener("focus", codesCursor);
-    document.getElementById("charsText").addEventListener("input", () => {
+    charsText = document.getElementById("charsText");
+    codesText = document.getElementById("codesText");
+    selectBlock = document.getElementById("selectBlock");
+    charsBlock = document.getElementById("charsBlock");
+
+    renderChars();
+    charsText.value = ""; // Firefox pre-loads previous value on refresh, introducing bugs, so clear it
+
+    charsText.addEventListener("click", codesCursor);
+    charsText.addEventListener("input", () => {
         codesRender();
         codesCursor();
     });
-    document.getElementById("charsText").addEventListener("selectionchange", codesCursor);
+    charsText.addEventListener("selectionchange", codesCursor);
 
     document.getElementById("nfc").addEventListener("click", () => {
-        document.getElementById("charsText").value = document.getElementById("charsText").value.normalize("NFC")
+        charsText.value = charsText.value.normalize("NFC")
         codesRender();
-    })
+    });
     document.getElementById("nfd").addEventListener("click", () => {
-        document.getElementById("charsText").value = document.getElementById("charsText").value.normalize("NFD")
+        charsText.value = charsText.value.normalize("NFD")
         codesRender();
-    })
+    });
     document.getElementById("nfkc").addEventListener("click", () => {
-        document.getElementById("charsText").value = document.getElementById("charsText").value.normalize("NFKC")
+        charsText.value = charsText.value.normalize("NFKC")
         codesRender();
-    })
+    });
     document.getElementById("nfkd").addEventListener("click", () => {
-        document.getElementById("charsText").value = document.getElementById("charsText").value.normalize("NFKD")
+        charsText.value = charsText.value.normalize("NFKD")
         codesRender();
+    });
+    document.getElementById("insertCodeButton").addEventListener("click", () => {
+        let val = document.getElementById("insertCode").value.trim();
+        if (val.startsWith("U+"))
+            val = val.slice(2);
+        let cp = parseInt(val, 16);
+        if (isNaN(cp))
+            insertChar("�")
+        else
+            insertChar(String.fromCodePoint(cp));
+    });
+    document.getElementById("search").addEventListener("input", () => {
+        let option = Array.from(selectBlock.options)
+                        .find(option => option.textContent.toLowerCase().startsWith(document.getElementById("search").value.toLowerCase()));
+        if (option !== undefined)
+            option.selected = "selected";
+        renderChars();
     })
-
+    selectBlock.addEventListener("change", () => {
+        renderChars();
+    });
 }
